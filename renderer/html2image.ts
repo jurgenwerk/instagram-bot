68df74b8ad29565bf349cdd883c5b7b4ea2d9708
Matic Jurglič <matic@jurglic.si>
2022-03-20 21:02:21 +0100

    Pupetter flags for docker

--- renderer/html2image.ts
+++ renderer/html2image.ts
@@ -1,7 +1,9 @@
 import puppeteer from "puppeteer";
 
 export default async function html2image(html: string): Promise<Buffer> {
-  let browser = await puppeteer.launch();
+  let browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   let page = await browser.newPage();
   await page.setViewport({
     width: 800,
